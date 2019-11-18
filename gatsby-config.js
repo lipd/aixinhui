69703152c80c703b5e5075fac37a6b55c1a7aa89
lipd <lipeidao@gmail.com>
2019-11-18 21:50:27 +0800

    nav-bar text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,34 @@
 module.exports = {
   siteMetadata: {
     navbarLinks: [
-      {to: "/makeup", name: "Makeup"},
-      {to: "/lifestyle", name: "Lifestyle"},
-      {to: "/blog", name: "blog"},
+      { to: "/makeup", name: "机构介绍" },
+      { to: "/lifestyle", name: "“蝶音” @ 艺术科技指导" },
+      { to: "/blog", name: "音乐剧师资培训" },
+      { to: "/blog", name: "招生与夏令营" },
+      { to: "/blog", name: " 资源与服务" },
+      { to: "/blog", name: " 演出与活动" }
     ],
-    title: "TYRA",
+    title: "爱星汇",
     description: "Tyra is a fast, feminine, and chic Gatsby.js theme.",
     siteUrl: "https://tyra-starter.netlify.com",
     homepageHeader: "Welcome to Your New Blog",
-    homepageAbout: "Tyra is a modern, sleek and feminine Gatsby.js theme. Easily create a beautiful and fast blog and draw attention to your stellar content.",
+    homepageAbout:
+      "Tyra is a modern, sleek and feminine Gatsby.js theme. Easily create a beautiful and fast blog and draw attention to your stellar content.",
     mailChimpUrl: "https://mailchimp.com",
     mailChimpToken: "MAILCHIMP TOKEN HERE",
     youtube: "", // YOUR YOUTUBE PROFILE HERE
-    github: "", // YOUR GITHUB PROFILE HERE 
+    github: "", // YOUR GITHUB PROFILE HERE
     pinterest: "", // YOUR PINTEREST PROFILE HERE
     facebook: "", // YOUR FACEBOOK PROFILE HERE
-    twitter: "", // YOUR TWITTER PROFILE HERE
+    twitter: "" // YOUR TWITTER PROFILE HERE
   },
   plugins: [
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
         {
@@ -47,9 +51,9 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
             },
             query: `
             {
@@ -73,50 +77,50 @@ module.exports = {
             }
           `,
             output: "/rss.xml",
-            title: "Gatsby RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content`,
-        name: 'content',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1400,
-            },
-          },
-        ],
+            title: "Gatsby RSS Feed"
+          }
+        ]
       }
     },
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/content`,
+        name: "content"
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-copy-linked-files",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1400
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ['Karla', 'Playfair Display', 'Lora']
+          families: ["Karla", "Playfair Display", "Lora"]
         }
       }
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "",
         head: false,
         anonymize: true,
         respectDNT: true,
-        exclude: ['/success'],
-        cookieDomain: "tyra-starter.netlify.com",
+        exclude: ["/success"],
+        cookieDomain: "tyra-starter.netlify.com"
       }
     }
   ]
-}
+};
